aeb6ec9c61ceae05c4a648d322b9081b33fe855d
Subham Raj <glorious10info@gmail.com>
2023-04-01 22:58:40 +0530

    Helper functions added to the javascript file.

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-const btn = document.querySelector(".submit")
+const submitBtn = document.querySelector("#submit")
+const input = document.querySelector("#input")
+const errorSpan = document.querySelector("#error")
+const resultsContainer = document.querySelector("#results")
 
+const disabled = () => {
+    input.disabled = true
+    submitBtn.disabled = true
+}
+
+const enabled = () => {
+    input.disabled = false
+    submitBtn.disabled = false
+}
+
+const clearPreviousResults = () => {
+    resultsContainer.innerHTML = ''
+    errorSpan.innerHTML = ''
+}
+
+const isInputEmpty = (input) => {
+    if(!input || input == '')
+    {
+        return true;
+    }
+    else return false;
+}
+
+const showError = () => {
+    errorSpan.innerHTML = `${error}`
+}
